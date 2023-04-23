22e681190d1806fc0be4ba41b0c52ef24be967f9
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-04-23 13:36:24 +0000

    Implemented XDG new_toplevel and XDG decorations

--- src/compositor/wayland/handlers/xdg.rs
+++ src/compositor/wayland/handlers/xdg.rs
@@ -1,14 +1,18 @@
 use smithay::{
     delegate_xdg_activation, delegate_xdg_decoration, delegate_xdg_shell,
+    desktop::Window,
     reexports::{
-        wayland_protocols::xdg::decoration::zv1::server::zxdg_toplevel_decoration_v1::Mode,
+        wayland_protocols::xdg::{
+            decoration::zv1::server::zxdg_toplevel_decoration_v1::Mode, shell::server::xdg_toplevel,
+        },
         wayland_server::protocol::{wl_seat::WlSeat, wl_surface::WlSurface},
     },
     utils::Serial,
     wayland::{
+        compositor::with_states,
         shell::xdg::{
             decoration::XdgDecorationHandler, PopupSurface, PositionerState, ToplevelSurface,
-            XdgShellHandler, XdgShellState,
+            XdgShellHandler, XdgShellState, XdgToplevelSurfaceData,
         },
         xdg_activation::{
             XdgActivationHandler, XdgActivationState, XdgActivationToken, XdgActivationTokenData,
@@ -20,7 +24,7 @@ use crate::{backend::Backend, state::NoWayState};
 
 impl<BackendData: Backend + 'static> XdgActivationHandler for NoWayState<BackendData> {
     fn activation_state(&mut self) -> &mut XdgActivationState {
-        todo!()
+        &mut self.compositor.xdg_activation_state
     }
 
     fn request_activation(
@@ -46,15 +50,53 @@ delegate_xdg_activation!(@<BackendData: Backend + 'static> NoWayState<BackendDat
 
 impl<BackendData: Backend + 'static> XdgDecorationHandler for NoWayState<BackendData> {
     fn new_decoration(&mut self, toplevel: ToplevelSurface) {
-        todo!()
+        toplevel.with_pending_state(|state| {
+            state.decoration_mode = Some(Mode::ClientSide);
+        });
     }
 
     fn request_mode(&mut self, toplevel: ToplevelSurface, mode: Mode) {
-        todo!()
+        toplevel.with_pending_state(|state| {
+            state.decoration_mode = Some(mode);
+            // state.decoration_mode = Some(match mode {
+            //     DecorationMode::ServerSide => Mode::ServerSide,
+            //     _ => Mode::ClientSide,
+            // });
+        });
+
+        let initial_configure_sent = with_states(toplevel.wl_surface(), |states| {
+            states
+                .data_map
+                .get::<XdgToplevelSurfaceData>()
+                .unwrap()
+                .lock()
+                .unwrap()
+                .initial_configure_sent
+        });
+
+        if initial_configure_sent {
+            toplevel.send_configure();
+        }
     }
 
     fn unset_mode(&mut self, toplevel: ToplevelSurface) {
-        todo!()
+        toplevel.with_pending_state(|state| {
+            state.decoration_mode = Some(Mode::ClientSide);
+        });
+
+        let initial_configure_sent = with_states(toplevel.wl_surface(), |states| {
+            states
+                .data_map
+                .get::<XdgToplevelSurfaceData>()
+                .unwrap()
+                .lock()
+                .unwrap()
+                .initial_configure_sent
+        });
+
+        if initial_configure_sent {
+            toplevel.send_configure();
+        }
     }
 }
 
@@ -62,11 +104,19 @@ delegate_xdg_decoration!(@<BackendData: Backend + 'static> NoWayState<BackendDat
 
 impl<BackendData: Backend + 'static> XdgShellHandler for NoWayState<BackendData> {
     fn xdg_shell_state(&mut self) -> &mut XdgShellState {
-        todo!()
+        &mut self.compositor.xdg_shell_state
     }
 
     fn new_toplevel(&mut self, surface: ToplevelSurface) {
-        todo!()
+        let window = Window::new(surface);
+        window.toplevel().with_pending_state(|state| {
+            state.states.set(xdg_toplevel::State::TiledLeft);
+            state.states.set(xdg_toplevel::State::TiledBottom);
+            state.states.set(xdg_toplevel::State::TiledRight);
+            state.states.set(xdg_toplevel::State::TiledTop);
+        });
+        window.toplevel().send_configure();
+        self.compositor.space.map_element(window, (0, 0), false);
     }
 
     fn new_popup(&mut self, surface: PopupSurface, positioner: PositionerState) {
